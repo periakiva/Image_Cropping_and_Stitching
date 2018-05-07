194a173899e9017e6b91e1dda5f2d4f4f0e88b2d
periakiva <periha@gmail.com>
2018-05-07 13:29:13 -0400

    updates to single crop-stitch

--- Image-cropping-pipeline.py
+++ Image-cropping-pipeline.py
@@ -24,7 +24,7 @@ class data_loader(object):
         self.row_crop_range= [i for i in range(0,2401,80)]
         self.col_crop_range= [j for j in range(0,2401,80)]
 
-    def crop_single_image(self,image_number):
+    def crop_single_image(self):
         #self.cropped_data[image_number]=[] # list of cropped images. Assume images 2448*2448*3
         self.cropped_data=[] # list of cropped images. Assume images 2448*2448*3
 
@@ -43,14 +43,14 @@ class data_loader(object):
       #  for image_number in self.images_data_list.keys():
             #self.crop_single_image(image_number)
             
-    def stich_single_image(self,image_number):
+    def stich_single_image(self):
         stiched = np.zeros((2400,2400,3))
         counter=0
         i_crop = self.row_crop_range
         j_crop = self.col_crop_range
         for col in range(0,29):
             for row in range(0,29):
-                temp=self.cropped_data[image_number][counter]
+                temp=self.cropped_data[counter]
                 stiched[i_crop[row]:i_crop[row+2],j_crop[col]:j_crop[col+2],:]=stiched[i_crop[row]:i_crop[row+2],j_crop[col]:j_crop[col+2],:] + temp
                 counter=counter+1
 
@@ -60,11 +60,13 @@ class data_loader(object):
         stiched[80:2320,0:80,:]=stiched[80:2320,0:80,:]//2
         stiched[80:2320,2320:2400,:]=stiched[80:2320,2320:2400,:]//2
         mpimg.imsave("stiched %s.png"%(image_number), stiched)
+        #self.stiched_data[image_number]=stiched
         self.stiched_data[image_number]=stiched
+
         
-    def stich_all_images(self):
-        for image_number in self.cropped_data.keys():
-            self.stich_single_image(image_number)
+    #def stich_all_images(self):
+       # for image_number in self.cropped_data.keys():
+         #   self.stich_single_image(image_number)
 
 
 # In[9]:
